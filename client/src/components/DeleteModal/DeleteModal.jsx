f8982e2e08cbcb3c214789715e521cd7e5785454
nmargenov <nikolay_margenov@yahoo.com>
2023-11-13 18:13:30 +0200

    added spinner for delete

--- client/src/components/DeleteModal/DeleteModal.jsx
+++ client/src/components/DeleteModal/DeleteModal.jsx
@@ -1,20 +1,25 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from "../shared/addEditTodoModal.module.css";
 import { ModalContext } from "../../contexts/ModalContext";
 import { deleteTodo } from "../../services/todoService";
 import { TodoContext } from "../../contexts/TodoContext";
+import { SmallSpinner } from "../TableList/TodoListItem/SmallSpinner/SmallSpinner";
 
 export const DeleteModal = () => {
+  const [isDeleting, setIsDeleting] = useState(false);
   const { onDeleteClose, deleteId } = useContext(ModalContext);
   const { setTodos } = useContext(TodoContext);
 
   function onDeleteClick() {
+    setIsDeleting(true);
     deleteTodo(deleteId)
       .then((data) => {
+        setIsDeleting(false);
         setTodos((oldState) => oldState.filter((t) => t._id !== data._id));
         onDeleteClose();
       })
       .catch((err) => {
+        setIsDeleting(false);
         console.log(err);
       });
   }
@@ -29,12 +34,20 @@ export const DeleteModal = () => {
           <h2>Are you sure you want to delete this task?</h2>
         </main>
         <footer>
-          <a onClick={() => onDeleteClose()} className={styles["cancel-btn"]}>
-            Cancel
-          </a>
-          <a onClick={onDeleteClick} className={styles["save-btn"]}>
-            Delete
-          </a>
+          {isDeleting && <SmallSpinner/>}
+          {!isDeleting &&
+            <>
+              <a
+                onClick={() => onDeleteClose()}
+                className={styles["cancel-btn"]}
+              >
+                Cancel
+              </a>
+              <a onClick={onDeleteClick} className={styles["save-btn"]}>
+                Delete
+              </a>
+            </>
+          }
         </footer>
       </div>
     </div>
